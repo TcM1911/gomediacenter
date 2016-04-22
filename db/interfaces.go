8d415ed07b9883acdcf4cc587880df6d26d03770
TcM1911 <mr.cableman@gmail.com>
2016-04-22 19:47:52 +0100

    Add docs.

--- db/interfaces.go
+++ db/interfaces.go
@@ -2,6 +2,7 @@ package db
 
 import "github.com/tcm1911/gomediacenter"
 
+// Database is the interface for the database.
 type Database interface {
 	Connect(host string)
 	Close()
@@ -10,15 +11,18 @@ type Database interface {
 	IntroFinder
 }
 
+// ItemFinder can find an item in the database.
 type ItemFinder interface {
 	FindItemById(id string) (gomediacenter.MEDIATYPE, interface{}, error)
 	FindItemUserData(uid, itemId string) (*gomediacenter.ItemUserData, error)
 }
 
+// IntroFinder can find intros for an item.
 type IntroFinder interface {
 	FindItemIntro(id string) (*[]gomediacenter.Intro, error)
 }
 
+// ItemUserData can find user data for an item.
 type ItemUserData interface {
 	InsertItemUserData(uid, itemId string) error
 	DeleteItemUserData(uid, itemId string) error
