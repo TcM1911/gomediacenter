cd9f1574184532127bdc1001738d29ff4377484b
TcM1911 <mr.cableman@gmail.com>
2016-09-26 00:04:40 +0100

    Create new id struct

---
+++ id_test.go
@@ -0,0 +1,61 @@
+package gomediacenter
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewID(t *testing.T) {
+	id := NewID()
+	expected := ID{}
+	assert.IsType(t, expected.uuid, id.uuid)
+}
+
+func TestNewRandomID(t *testing.T) {
+	id := NewRandomID()
+	expected := ID{}
+	assert.IsType(t, expected.uuid, id.uuid)
+}
+
+func TestIDStringer(t *testing.T) {
+	id := NewID()
+	assert.IsType(t, "", id.String())
+}
+
+func TestIDFromString(t *testing.T) {
+	assert := assert.New(t)
+	expected := "00112233445566778899aabbccddeeff"
+	id, err := IDFromString(expected)
+	assert.NoError(err, "Should not return an error.")
+	assert.IsType(ID{}, id, "Should be an ID type.")
+	assert.Equal(expected, id.String(), "Should return expected ID string.")
+}
+
+func TestIDFromBytes(t *testing.T) {
+	assert := assert.New(t)
+	expected := "00112233445566778899aabbccddeeff"
+	b := make([]byte, 16)
+	hex.Decode(b, []byte(expected))
+	id, err := IDFromBytes(b)
+	assert.NoError(err, "Should not return an error.")
+	assert.IsType(ID{}, id, "Should be an ID type.")
+	assert.Equal(expected, id.String(), "Should return expected ID string.")
+}
+
+func TestIDFromBytesToLong(t *testing.T) {
+	assert := assert.New(t)
+	toLong := "00112233445566778899aabbccddeeffaa"
+	b := make([]byte, 17)
+	hex.Decode(b, []byte(toLong))
+	_, err := IDFromBytes(b)
+	assert.Error(err, "Should return an error.")
+}
+
+func TestIDFromStringToLong(t *testing.T) {
+	assert := assert.New(t)
+	toLong := "00112233445566778899aabbccddeeffaa"
+	_, err := IDFromString(toLong)
+	assert.Error(err, "Should return an error.")
+}
